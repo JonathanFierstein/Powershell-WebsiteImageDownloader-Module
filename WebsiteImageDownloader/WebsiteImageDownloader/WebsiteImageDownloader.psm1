a3a94ad89ea79ac071f5e7528655498ba30fa1e8
Jonathan Fierstein <jonathan.fierstein@gmail.com>
2024-06-26 16:14:05 -0700

    Updated .gitignore to include other powershell studio temp files...
    	Also added a module directory and empty/starter module files
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   WebsiteImageDownloader/WebsiteImageDownloader/Test-Module.ps1
    	new file:   WebsiteImageDownloader/WebsiteImageDownloader/WebsiteImageDownloader.psd1
    	new file:   WebsiteImageDownloader/WebsiteImageDownloader/WebsiteImageDownloader.psm1

---
+++ WebsiteImageDownloader/WebsiteImageDownloader/WebsiteImageDownloader.psm1
@@ -0,0 +1,20 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.213
+	 Created on:   	6/26/2024 4:06 PM
+	 Created by:   	Night
+	 Organization: 	
+	 Filename:     	WebsiteImageDownloader.psm1
+	-------------------------------------------------------------------------
+	 Module Name: WebsiteImageDownloader
+	===========================================================================
+#>
+
+
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
+
