516f6ba893a876129b748399b8e2ac9e4b5ec336
Jonathan Fierstein <48401556+JonathanFierstein@users.noreply.github.com>
2024-07-05 23:16:30 -0700

    Update WebsiteImageDownloader.psm1
        Updated "Created by:" comment section to accurately show my name and contact email address

--- WebsiteImageDownloader/WebsiteImageDownloader.psm1
+++ WebsiteImageDownloader/WebsiteImageDownloader.psm1
@@ -2,15 +2,16 @@
 	===========================================================================
 	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.213
 	 Created on:   	6/26/2024 4:06 PM
-	 Created by:   	Night
+	 Created by:   	Nightshade - Jonathan Fierstein 
+         (jonathan.fierstein@gmail.com)
 	 Organization: 	
 	 Filename:     	WebsiteImageDownloader.psm1
 	-------------------------------------------------------------------------
 	 Module Name: WebsiteImageDownloader
-        -------------------------------------------------------------------------
+    -------------------------------------------------------------------------
 	 Notes:
 
-	This is the core module file for the WebsiteImageDownloader module.
+	 This is the core module file for the WebsiteImageDownloader module.
 	===========================================================================
 #>
 
